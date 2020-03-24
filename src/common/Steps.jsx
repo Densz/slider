6f92b17dd24d77abff9d098b5cb685c909a41124
Denis ZHENG <zheng.denis@gmail.com>
2020-03-24 12:57:22 +0100

    [FEATURE] Style dot mark individually

--- src/common/Steps.jsx
+++ src/common/Steps.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import classNames from 'classnames';
-import warning from 'warning';
+import React from "react";
+import classNames from "classnames";
+import warning from "warning";
 
 const calcPoints = (vertical, marks, dots, step, min, max) => {
   warning(
     dots ? step > 0 : true,
-    '`Slider[step]` should be a positive number in order to make Slider[dots] work.'
+    "`Slider[step]` should be a positive number in order to make Slider[dots] work."
   );
-  const points = Object.keys(marks).map(parseFloat).sort((a, b) => a - b);
+  const points = Object.keys(marks)
+    .map(parseFloat)
+    .sort((a, b) => a - b);
   if (dots && step) {
     for (let i = min; i <= max; i += step) {
       if (points.indexOf(i) === -1) {
@@ -18,28 +20,53 @@ const calcPoints = (vertical, marks, dots, step, min, max) => {
   return points;
 };
 
-const Steps = ({ prefixCls, vertical, reverse, marks, dots, step, included,
-  lowerBound, upperBound, max, min, dotStyle, activeDotStyle }) => {
+const Steps = ({
+  prefixCls,
+  vertical,
+  reverse,
+  marks,
+  dots,
+  step,
+  included,
+  lowerBound,
+  upperBound,
+  max,
+  min,
+  dotStyle,
+  activeDotStyle
+}) => {
   const range = max - min;
-  const elements = calcPoints(vertical, marks, dots, step, min, max).map((point) => {
-    const offset = `${Math.abs(point - min) / range * 100}%`;
+  const elements = calcPoints(vertical, marks, dots, step, min, max).map(
+    point => {
+      const offset = `${(Math.abs(point - min) / range) * 100}%`;
 
-    const isActived = (!included && point === upperBound) ||
-            (included && point <= upperBound && point >= lowerBound);
-    let style = vertical ? { ...dotStyle, [reverse ? 'top' :'bottom']: offset }
-      : { ...dotStyle, [reverse ? 'right' :'left']: offset };
-    if (isActived) {
-      style = { ...style, ...activeDotStyle };
+      const isActived =
+        (!included && point === upperBound) ||
+        (included && point <= upperBound && point >= lowerBound);
+      let style = vertical
+        ? {
+            ...dotStyle,
+            [reverse ? "top" : "bottom"]: offset,
+            ...marks[point].dotStyle
+          }
+        : {
+            ...dotStyle,
+            [reverse ? "right" : "left"]: offset,
+            ...marks[point].dotStyle
+          };
+      if (isActived) {
+        style = { ...style, ...activeDotStyle, ...marks[point].dotStyle };
+      }
+
+      const pointClassName = classNames({
+        [`${prefixCls}-dot`]: true,
+        [`${prefixCls}-dot-active`]: isActived,
+        [`${prefixCls}-dot-reverse`]: reverse
+      });
+
+      return <span className={pointClassName} style={style} key={point} />;
     }
-
-    const pointClassName = classNames({
-      [`${prefixCls}-dot`]: true,
-      [`${prefixCls}-dot-active`]: isActived,
-      [`${prefixCls}-dot-reverse`]: reverse,
-    });
-
-    return <span className={pointClassName} style={style} key={point} />;
-  });
+  );
 
   return <div className={`${prefixCls}-step`}>{elements}</div>;
 };
